f92f29e35032f9a98e1d355fc0f65337286e9014
Nicolás Felipe Dipierro <dipixd@gmail.com>
2024-09-08 16:01:36 -0300

    +prettier, create sprite shapes with Voronoi sections *gitignore

--- Assets/Scripts/Voronoi.cs
+++ Assets/Scripts/Voronoi.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,6 +12,7 @@ public class Voronoi : MonoBehaviour
     private Texture2D _voronoiTexture;
     private Vector2[] _points;
     private Color[] _colors;
+    private List<Vector2>[] _regionPixels;    
 
     private void Start()
     {
@@ -22,9 +24,9 @@ public class Voronoi : MonoBehaviour
         GeneratePoints();
         GenerateVoronoiTexture();
         GenerateOverlay();
+        GenerateSprites();
         ApplyTextureToUI();
     }
-
 
     private void GeneratePoints()
     {
@@ -41,6 +43,12 @@ public class Voronoi : MonoBehaviour
     private void GenerateVoronoiTexture()
     {
         _voronoiTexture = new Texture2D(_textureWidth, _textureHeight);
+        _regionPixels = new List<Vector2>[_pointCount];
+        
+        for (int i = 0; i < _pointCount; i++)
+        {
+            _regionPixels[i] = new List<Vector2>();
+        }
 
         for (int y = 0; y < _textureHeight; y++)
         {
@@ -60,11 +68,54 @@ public class Voronoi : MonoBehaviour
                     }
                 }
 
+                _regionPixels[closestPointIndex].Add(pixelPos);
                 _voronoiTexture.SetPixel(x, y, _colors[closestPointIndex]);
             }
         }
 
         _voronoiTexture.Apply();
+    }
+
+    private Mesh CreateMeshForRegion(List<Vector2> regionPixels)
+    {
+        Mesh mesh = new Mesh();
+        Vector3[] vertices = new Vector3[regionPixels.Count];
+        int[] triangles = new int[(regionPixels.Count - 2) * 3];
+
+        for (int i = 0; i < regionPixels.Count; i++)
+        {
+            vertices[i] = new Vector3(regionPixels[i].x, regionPixels[i].y, 0);
+        }
+
+        // Simple triangulation for the region
+        for (int i = 0; i < regionPixels.Count - 2; i++)
+        {
+            triangles[i * 3] = 0;
+            triangles[i * 3 + 1] = i + 1;
+            triangles[i * 3 + 2] = i + 2;
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+
+    private void GenerateSprites()
+    {
+        for (int i = 0; i < _regionPixels.Length; i++)
+        {
+            Mesh mesh = CreateMeshForRegion(_regionPixels[i]);
+
+            GameObject regionObject = new GameObject($"Region_{i}");
+            MeshFilter meshFilter = regionObject.AddComponent<MeshFilter>();
+            MeshRenderer meshRenderer = regionObject.AddComponent<MeshRenderer>();
+
+            meshFilter.mesh = mesh;
+            meshRenderer.material = new Material(Shader.Find("Sprites/Default"));
+            meshRenderer.material.color = _colors[i];
+        }
     }
 
     private void GenerateOverlay()
